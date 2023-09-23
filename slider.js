9649227d65c27d55dcc29cd453e1d4fd9086032a
ChristianUwU <obama060296@gmail.com>
2023-09-22 21:12:51 -0400

    Primera pagina web

---
+++ slider.js
@@ -0,0 +1,51 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider-section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+
+// BTN RIGHT 
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+
+// BTN LEFT 
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider-section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener("click", function(){
+Next();
+});
+
+
+btnLeft.addEventListener("click", function(){
+    Prev();
+});
+
+
+setInterval(function(){
+    Next();
+}, 4000);
+
